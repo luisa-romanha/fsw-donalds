2f9e73826fca72341fdc7bf58ba536bf1b44cd74
Luisa Romanha <romanhaluisa@gmail.com>
2025-02-23 04:18:33 -0300

    feat: add cart functionalities and add ensure order can be created

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,9 @@
-
 import "./globals.css";
 
 import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
+
+import { Toaster } from "@/components/ui/sonner";
 
 import { CartProvider } from "./[slug]/menu/contexts/cart";
 
@@ -25,6 +26,8 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${poppins.className} antialiased`}>
         <CartProvider>{children}</CartProvider>
+
+        <Toaster/>
       </body>
     </html>
   );
